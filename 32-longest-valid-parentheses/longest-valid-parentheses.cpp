a26574c0046baa1783875914d45f805788af7ce2
dc <74961311+dc1306@users.noreply.github.com>
2026-08-10 01:35:28 +0530

    Time: 617 ms (9.00%) | Memory: 14.1 MB (5.34%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -1,50 +1,6 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        // int n = s.size();
-        // int l = 0;
-        // int max_len = 0;
-        // int curr = 0;
-        // int open = 0;
-        // int close = 0;
-
-        // while(curr < s.size()){
-        //     if(s[curr] == '(')open++;
-        //     else close ++;
-
-        //     if(close == open){
-        //         max_len = max(max_len, curr - l + 1);
-        //     }
-
-        //     if(close > open){
-        //         if(s[l] == '(')open--;
-        //         else close--;
-        //         l++;
-        //     }
-
-        //     curr++; 
-        // }
-        // l = n-1;
-        // curr = n-1;
-        // open = 0;
-        // close = 0;
-        // while(curr >= 0){
-        //     if(s[curr] == '(')open++;
-        //     else close ++;
-
-        //     if(close == open){
-        //         max_len = max(max_len, l - curr + 1);
-        //     }
-
-        //     if(close < open){
-        //         if(s[l] == '(')open--;
-        //         else close--;
-        //         l--;
-        //     }
-
-        //     curr--; 
-        // }
-        // return max_len;
         int n = s.size();
         stack <pair<char, int>> st;
         int max_len = 0;
