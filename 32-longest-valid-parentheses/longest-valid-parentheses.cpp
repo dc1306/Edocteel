1f54d33cbe23d9a3a82382edf3abb2a49d05412f
dc <74961311+dc1306@users.noreply.github.com>
2026-08-10 01:16:39 +0530

    Time: 610 ms (9.00%) | Memory: 13.8 MB (5.34%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        // int l = 0;
+        // int max_len = 0;
+        // int curr = 0;
+        // int open = 0;
+        // int close = 0;
+
+        // while(curr < s.size()){
+        //     if(s[curr] == '(')open++;
+        //     else close ++;
+
+        //     if(close == open){
+        //         max_len = max(max_len, curr - l + 1);
+        //     }
+
+        //     if(close > open){
+        //         if(s[l] == '(')open--;
+        //         else close--;
+        //         l++;
+        //     }
+
+        //     curr++; 
+        // }
+        // while(open > close && l < s.size()-1){
+        //     if(s[l] == '(')open--;
+        //     else close--;
+        //     l++;
+        //     if(close == open){
+        //         max_len = max(max_len, curr - l);
+        //     }
+        // }
+        // return max_len;
+        int n = s.size();
+        stack <pair<char, int>> st;
+        int max_len = 0;
+        st.push({'!',-1});
+        for(int i = 0;i < n;i ++){
+            if(st.top().first == '!' || s[i] == st.top().first || s[i] == '(')st.push({s[i],i});
+            else st.pop();
+        }
+        st.push({'!', n});
+
+        while(!(st.empty())){
+            int temp = st.top().second;
+            cout << temp << endl;
+            st.pop();
+            if(!(st.empty())){
+                max_len = max(max_len, temp - st.top().second - 1);
+                cout << temp << endl;
+            }
+        }
+        return max_len;
+    }
+};
